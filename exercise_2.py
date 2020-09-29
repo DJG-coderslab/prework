c622e70e082f24cbe816988640463e07513d4f68
JG <coderslab.sjg@pwr.pl>
2020-09-29 20:39:51 +0200

    Zadanie nr 2, wartości logiczne

---
+++ exercise_2.py
@@ -0,0 +1,10 @@
+var_1 = True
+var_2 = True
+var_3 = False
+Var_4 = False
+
+var_1 == Var_2  # => True
+var_1 == Var_3  # => False
+var_3 == var_4  # => True
+
+"""Nie bardzo wiem jak lepiej to skomentować"""
