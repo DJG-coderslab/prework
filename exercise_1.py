0d5c39658c2a8c850006d040271c7d091dddf190
JG <coderslab.sjg@pwr.pl>
2020-09-29 20:34:27 +0200

    Zadanie 3 i uzupełnienie zadania 1
    W pliku exercise_1 dodane zadanie z typów danych.

--- exercise_1.py
+++ exercise_1.py
@@ -1,1 +1,14 @@
 print("Mam na imię Jarek")
+
+var_1 = 100
+var_2 = 100.0
+var_3 = "100"
+var_4 = "100\n100\n100"
+var_5 = True
+
+print(f"""
+zmienna var_1 typu {type (var_1)} -- liczba całkowita
+zmienna Var_2 typu {type (var_2)} -- liczba zmiennoprzecinkowa
+zmienna var_3 typu {type (var_3)} -- string
+zmienna var_4 typu {type (var_4)} -- wielolinijkowy napis
+zmienna var_5 typu {type (var_5)} -- wartość logiczna""")
---
+++ exercise_3.py
@@ -0,0 +1,17 @@
+In [9]: "Hello"
+Out[9]: 'Hello'
+
+In [10]: 'Hello'
+Out[10]: 'Hello'
+
+In [11]: 2
+Out[11]: 2
+
+In [12]: Hello
+---------------------------------------------------------------------------
+NameError                                 Traceback (most recent call last)
+<ipython-input-12-1d229271928d> in <module>
+----> 1 Hello
+
+NameError: name 'Hello' is not defined
+
