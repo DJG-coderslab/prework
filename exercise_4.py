8a1c8a6044cdd241f22c3952d0b1b2e07be0e3df
JG <coderslab.sjg@pwr.pl>
2020-09-29 21:16:51 +0200

    Zadanie nr 4 -- operacje matematyczne

---
+++ exercise_4.py
@@ -0,0 +1,15 @@
+a1 = None
+a2 = None
+
+a1 = 10
+a2 = 15
+sum_value = a1 + a2
+quotas = a2 / a1
+int_part = a2 // a1
+
+print(f"""
+a1 ({type(a1)}) = {a1}
+a2 ({type(a2)}) = {a2}
+quatas ({type(quotas)}) = {quotas}
+int_part ({type(int_part)}) = {int_part}
+""")
