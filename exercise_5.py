c8a44ddf9a5a1158883c365a7cc7359042c5a977
JG <coderslab.sjg@pwr.pl>
2020-09-29 21:25:20 +0200

    Zadanie nr 2 -- lista liczb

--- exercise_5.py
+++ exercise_5.py
@@ -1,3 +1,5 @@
+print("Zadanie nr 1")
+
 animals = []
 
 print(animals)
@@ -11,3 +13,7 @@ print(animals)
 animals.append("Kubuś Puchatek")
 print(animals)
 
+print("Zadanie nr 2")
+
+number_list = list(range(1, 9))
+print(f"Przedostatni element: {number_list[-2]}")
