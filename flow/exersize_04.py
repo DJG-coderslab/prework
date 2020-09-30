430f4734e3125848a428a6254ad5d613c0dc3e24
JG <coderslab.sjg@pwr.pl>
2020-09-30 12:28:24 +0200

    Flow, zadanie 4 -- porównanie liczb

---
+++ flow/exersize_04.py
@@ -0,0 +1,13 @@
+""" Zadanie 4 – Porównanie liczb """
+
+a = input("Podaj pierwsą liczbę: ")
+b = input("Podaj drugą liczbę: ")
+
+a = float(a)
+b = float(b)
+
+if a > b:
+    print (f"a ({a}) jest większe!")
+else:
+    print(f"b ({b}) jest większe")
+
