a962f4263eb142e1fc12fa54c51e4c000be8e5ad
JG <coderslab.sjg@pwr.pl>
2020-09-30 19:11:10 +0200

    Literówka

--- exercise_4.py
+++ exercise_4.py
@@ -10,6 +10,6 @@ int_part = a2 // a1
 print(f"""
 a1 ({type(a1)}) = {a1}
 a2 ({type(a2)}) = {a2}
-quatas ({type(quotas)}) = {quotas}
+qouatas ({type(quotas)}) = {quotas}
 int_part ({type(int_part)}) = {int_part}
 """)
