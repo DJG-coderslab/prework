6c9ce0a91de796d768bc426d7b3301ee7931da0f
JG <coderslab.sjg@pwr.pl>
2020-09-30 09:47:56 +0200

    Std lib zadanie nr 1 -- pobieranie danych

---
+++ std_lib/exercise_01.py
@@ -0,0 +1,11 @@
+name = input("Podaj imię: ")
+sure_name = input("Podaj nazwisko: ")
+
+print(name + " " + sure_name + " jest programistą Pythona!")
+
+
+# lub, tak mi się bardziej podoba
+
+print(f"{name} {sure_name} jest programistą Pythona!")
+
+
